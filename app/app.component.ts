0f144821bc847ad22bc014f8d7fb976b4149212f
Bobby Montalvo <bobby.montalvo@gmail.com>
2016-10-11 22:30:56 -0400

    Working. Finished Step 7.

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: '<h1>My First Angular App</h1>'
+  template: '<h1>My SECOND Angular App</h1>'
 })
 
 export class AppComponent { }
